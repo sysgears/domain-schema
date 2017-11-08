c0d1540a1bf923f94250ece5d2e157d261dfc1d1
Victor Vlasenko <victor.vlasenko@sysgears.com>
2017-11-08 17:10:09 +0200

    Add simple jest tests

--- packages/domain-schema/src/__tests__/index.ts
+++ packages/domain-schema/src/__tests__/index.ts
@@ -1,7 +1,18 @@
-import DomainSchema from '../index';
+import DomainSchema, { Schema } from '../index';
 
 describe('DomainSchema', () => {
-  it('should do something', () => {
+  it('should instantiate from Schema class', () => {
+    new DomainSchema(class extends Schema {});
+  });
 
-  })
+  it('should instantiate from DomainSchema class', () => {
+    const schema = class extends Schema {};
+    const domainSchema = new DomainSchema(new DomainSchema(schema));
+    expect(domainSchema.schema).toEqual(new schema());
+  });
+
+  it('.name should match the name of the class', () => {
+    class MySchema extends Schema {}
+    expect(new DomainSchema(MySchema).name).toEqual(MySchema.name);
+  });
 });
