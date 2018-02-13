caf892f76fb02a0ccbed3a65924b76e28fdf5559
Evgenij Olshanskij <gekanchiko@gmail.com>
2018-02-13 18:23:38 +0200

    Refactor message providers

--- packages/domain-react-forms/src/index.ts
+++ packages/domain-react-forms/src/index.ts
@@ -1,17 +1,17 @@
 import DomainSchema from 'domain-schema';
 
-const supportedValidators = [
-  'required',
-  'match',
-  'maxLength',
-  'minLength',
-  'numberCheck',
-  'minValue',
-  'email',
-  'alphaNumeric',
-  'phoneNumber',
-  'equals'
-];
+const supportedValidators = {
+  required: { createMsg: () => `Required` },
+  match: { createMsg: comparableField => `Should match field '${comparableField}'` },
+  maxLength: { createMsg: max => `Must be ${max} characters or less` },
+  minLength: { createMsg: min => `Must be ${min} characters or more` },
+  numberCheck: { createMsg: () => `Must be a number` },
+  minValue: { createMsg: minValue => `Must be at least ${minValue}` },
+  email: { createMsg: () => `Invalid email address` },
+  alphaNumeric: { createMsg: () => `Only alphanumeric characters` },
+  phoneNumber: { createMsg: () => `Invalid phone number, must be 10 digits` },
+  equals: { createMsg: comparableValue => `Should match '${comparableValue}'` }
+};
 
 export default class DomainReactForms {
   constructor(private schema: DomainSchema) {}
@@ -28,7 +28,7 @@ export default class DomainReactForms {
           const s = schema[v];
           if (!s.type.isSchema) {
             Object.keys(s).forEach((validator: any) => {
-              if (!supportedValidators.includes(validator)) {
+              if (!supportedValidators[validator]) {
                 return;
               }
               let msg = null;
@@ -65,81 +65,46 @@ class Validators {
 
   // Non empty validation
   public static required = (value, msg, schemaValue) =>
-    schemaValue && !value ? msg || Validators.getRequiredText() : undefined;
+    schemaValue && !value ? msg || Validators.pickMsg('required') : undefined;
 
   // Match a particular field
   public static match = (value, msg, comparableField, values) =>
-    value !== values[comparableField] ? msg || Validators.getMatchText(comparableField) : undefined;
+    value !== values[comparableField] ? msg || Validators.pickMsg('match', comparableField) : undefined;
 
   // Max length validation
   public static maxLength = (value, msg, max) =>
-    value && value.length > max ? msg || Validators.getMaxLengthText(max) : undefined;
+    value && value.length > max ? msg || Validators.pickMsg('maxLength', max) : undefined;
 
   // Min length validation
   public static minLength = (value, msg, min) =>
-    value && value.length < min ? msg || Validators.getMinLengthText(min) : undefined;
+    value && value.length < min ? msg || Validators.pickMsg('minLength', min) : undefined;
 
   // Number validation
   public static numberCheck = (value, msg) =>
-    value && isNaN(Number(value)) ? msg || Validators.getNumberCheckText() : undefined;
+    value && isNaN(Number(value)) ? msg || Validators.pickMsg('numberCheck') : undefined;
 
   // Minimum value validation
   public static minValue = (value, msg, minValue) =>
-    value && value < minValue ? msg || Validators.getMinValueText(minValue) : undefined;
+    value && value < minValue ? msg || Validators.pickMsg('minValue', minValue) : undefined;
 
   // Email validation
   public static email = (value, msg) =>
-    value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? msg || Validators.getEmailText() : undefined;
+    value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? msg || Validators.pickMsg('email') : undefined;
 
   // Alpha numeric validation
   public static alphaNumeric = (value, msg) =>
-    value && /[^a-zA-Z0-9 ]/i.test(value) ? msg || Validators.getAlphaNumericText() : undefined;
+    value && /[^a-zA-Z0-9 ]/i.test(value) ? msg || Validators.pickMsg('alphaNumeric') : undefined;
 
   // Phone number validation
   public static phoneNumber = (value, msg) =>
-    value && !/^(0|[1-9][0-9]{9})$/i.test(value) ? msg || Validators.getPhoneNumberText() : undefined;
+    value && !/^(0|[1-9][0-9]{9})$/i.test(value) ? msg || Validators.pickMsg('phoneNumber') : undefined;
 
   // Equals validation
   public static equals = (value, msg, comparableValue) =>
-    value !== comparableValue ? msg || Validators.getEqualsText(comparableValue) : undefined;
+    value !== comparableValue ? msg || Validators.pickMsg('equals', comparableValue) : undefined;
 
-  private static getRequiredText = () => {
-    return Validators.messages.required || 'Required';
-  };
+  /* HELPERS */
 
-  private static getMatchText = comparableField => {
-    return Validators.messages.match || `Should match field '${comparableField}'`;
-  };
-
-  private static getMaxLengthText = max => {
-    return Validators.messages.maxLength || `Must be ${max} characters or less`;
-  };
-
-  private static getMinLengthText = min => {
-    return Validators.messages.minLength || `Must be ${min} characters or more`;
-  };
-
-  private static getNumberCheckText = () => {
-    return Validators.messages.numberCheck || 'Must be a number';
-  };
-
-  private static getMinValueText = minValue => {
-    return Validators.messages.minValue || `Must be at least ${minValue}`;
-  };
-
-  private static getEmailText = () => {
-    return Validators.messages.email || 'Invalid email address';
-  };
-
-  private static getAlphaNumericText = () => {
-    return Validators.messages.alphaNumeric || 'Only alphanumeric characters';
-  };
-
-  private static getPhoneNumberText = () => {
-    return Validators.messages.phoneNumber || 'Invalid phone number, must be 10 digits';
-  };
-
-  private static getEqualsText = comparableValue => {
-    return Validators.messages.equals || `Should match '${comparableValue}'`;
-  };
+  // Provides a message
+  private static pickMsg = (validator, txtSnippet) => supportedValidators[validator].createMsg(txtSnippet);
 }
