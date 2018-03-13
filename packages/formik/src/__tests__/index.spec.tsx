329ad60bf8493a069937026c8ab0b3d22c58cab3
Aleksey Zadorozhniy <zadorozhniyal@gmail.com>
2018-03-13 15:24:39 +0200

    Update tests

--- packages/formik/src/__tests__/index.spec.tsx
+++ packages/formik/src/__tests__/index.spec.tsx
@@ -12,26 +12,34 @@ describe('DomainFormik', () => {
         public username = {
           type: String,
           fieldType: FieldTypes.input,
-          label: 'Username',
-          placeholder: 'name',
+          input: {
+            label: 'Username',
+            placeholder: 'name'
+          },
           defaultValue: 'John'
         };
         public email = {
           type: String,
           fieldType: FieldTypes.input,
-          inputType: 'email',
-          label: 'Email'
+          input: {
+            type: 'email',
+            label: 'Email'
+          }
         };
         public pass = {
           type: String,
           fieldType: FieldTypes.input,
-          inputType: 'password',
-          label: 'Email'
+          input: {
+            type: 'password',
+            label: 'Email'
+          }
         };
         public isActive = {
           type: String,
           fieldType: FieldTypes.checkbox,
-          label: 'Is Active',
+          input: {
+            label: 'Is Active'
+          },
           defaultValue: true
         };
       }
@@ -49,7 +57,9 @@ describe('DomainFormik', () => {
       public firstName = {
         type: String,
         fieldType: FieldTypes.input,
-        label: 'First Name',
+        input: {
+          label: 'First Name'
+        },
         required: {
           value: true,
           msg: 'Required First Name'
@@ -58,7 +68,9 @@ describe('DomainFormik', () => {
       public lastName = {
         type: String,
         fieldType: FieldTypes.input,
-        label: 'Last Name',
+        input: {
+          label: 'Last Name'
+        },
         required: true
       };
     }
@@ -69,9 +81,11 @@ describe('DomainFormik', () => {
         public username = {
           type: String,
           fieldType: FieldTypes.input,
-          inputType: 'text',
-          name: 'name',
-          label: 'Username',
+          input: {
+            type: 'text',
+            name: 'name',
+            label: 'Username'
+          },
           defaultValue: 'User',
           required: true,
           validators: [
@@ -83,9 +97,11 @@ describe('DomainFormik', () => {
         public email = {
           type: String,
           fieldType: FieldTypes.input,
-          inputType: 'email',
-          label: 'Email',
-          placeholder: 'User email',
+          input: {
+            type: 'email',
+            label: 'Email',
+            placeholder: 'User email'
+          },
           required: true,
           email: true
         };
@@ -95,16 +111,20 @@ describe('DomainFormik', () => {
         public password = {
           type: String,
           fieldType: FieldTypes.input,
-          inputType: 'password',
-          label: 'Password',
+          input: {
+            type: 'password',
+            label: 'Password'
+          },
           required: true,
           min: 5
         };
         public passwordConfirmation = {
           type: String,
           fieldType: FieldTypes.input,
-          inputType: 'password',
-          label: 'Password Confirmation',
+          input: {
+            type: 'password',
+            label: 'Password Confirmation'
+          },
           matches: 'password'
         };
       }
@@ -131,51 +151,23 @@ describe('DomainFormik', () => {
         public username = {
           type: String,
           fieldType: FieldTypes.input,
-          inputType: 'text',
-          name: 'name',
-          label: 'Username',
+          input: {
+            inputType: 'text',
+            name: 'name',
+            label: 'Username'
+          },
           defaultValue: 'User'
         };
         public userfield = {
           type: String,
           fieldType: FieldTypes.custom,
           component: MyComponent,
-          attrs: {
+          input: {
             name: 'myfield',
             label: 'User Field',
             className: 'my-awesome-field'
           },
           defaultValue: 'my field'
-        };
-      }
-    );
-    const userForm = new DomainReactForms(schema);
-    const FormComponent = userForm.generateForm(() => null);
-    const component = renderer.create(<FormComponent />);
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('should generate form with custom attributes', () => {
-    const schema = new DomainSchema(
-      class User extends Schema {
-        public __ = { name: 'User' };
-        public id = DomainSchema.Int;
-        public email = {
-          type: String,
-          fieldType: FieldTypes.input,
-          inputType: 'email',
-          name: 'name',
-          label: 'Username',
-          defaultValue: 'User'
-        };
-        public secret = {
-          type: String,
-          fieldType: FieldTypes.input,
-          inputType: 'text',
-          label: 'Secret field',
-          hidden: true,
-          attributes: ['hidden']
         };
       }
     );
